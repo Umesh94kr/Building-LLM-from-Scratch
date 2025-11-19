0803265882ca6a5bb817e64388b3b9ea6da4c400
Umesh94kr <umeshkumar94628@gmail.com>
2025-11-19 23:49:32 +0530

    Loaded GPT2 weights

--- Stage2 - Training_and_Evaluation/transformer_blocks.py
+++ Stage2 - Training_and_Evaluation/transformer_blocks.py
@@ -161,5 +161,36 @@ class GPTModel(nn.Module):
         x = self.final_norm(x)
         logits = self.out_head(x)
         return logits
+    
+class GPT2Model(nn.Module):
+    def __init__(self, configs):
+        super().__init__()
+        # token embeddings
+        self.tok_emb = nn.Embedding(configs['vocab_size'], configs['emb_dim'])
+        # positionl encodings 
+        self.pos_emb = torch.nn.Embedding(configs['context_length'], configs['emb_dim'])
+        # dropout
+        self.drop_emb = nn.Dropout(configs['drop_rate'])
+
+        # transformer block - encoder only in case of GPT
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(configs) for _ in range(configs['n_layers'])]
+        )
+
+        self.final_norm = LayerNorm(configs)
+        self.out_head = nn.Linear(
+            configs["emb_dim"], configs["vocab_size"], bias=False
+        )
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+        x = tok_embeds + pos_embeds  # Shape [batch_size, num_tokens, emb_size]
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
 
         
